512593852b6148ddd7c2df5cfd18472cfdf82adb
andrewliebchen <andrewliebchen@gmail.com>
2015-09-27 10:52:55 -0700

    Infrastructure for file uploads to S3

---
+++ server/fileUploads.js
@@ -0,0 +1,15 @@
+Slingshot.createDirective('fileUploads', Slingshot.S3Storage, {
+  bucket: Meteor.settings.AWSBucketName,
+  acl: 'public-read',
+
+  authorize() {
+    if (!this.userId) {
+      throw new Meteor.Error(403, "Login Required");
+     }
+
+    return true;
+  },
+  key(file) {
+    return file.name;
+  }
+});
