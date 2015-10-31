dd8046d155058f1645916c6475d9e5646b2409a9
andrewliebchen <andrewliebchen@gmail.com>
2015-10-31 10:42:20 -0700

    Better image positioning

--- components/image.jsx
+++ components/image.jsx
@@ -10,14 +10,14 @@ Image = React.createClass({
 
   render() {
     return (
-      <div className="image__wrapper">
+      <div className="image">
         <a className="image__close" href="/">✖</a>
         <div className="image__container">
-          <div className="image">
+          <div className="image__main">
             <img src={this.data.image.src}/>
             {/*<ImageUploader parentId={this.data.image._id}/>*/}
           </div>
-          <aside class="image__aside">
+          <aside className="image__aside">
             <h1 className="image__title">{this.data.image.name}</h1>
             <CommentsList comments={this.data.comments} parentId={this.data.image._id}/>
           </aside>
