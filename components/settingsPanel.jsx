197cabdadef25e836d026f5a9eb0cdba53ce0646
Andrew Liebchen <andrewliebchen@gmail.com>
2015-11-25 09:57:27 -0800

    Delete project

--- components/settingsPanel.jsx
+++ components/settingsPanel.jsx
@@ -1,7 +1,25 @@
 SettingsPanel = React.createClass({
+  handleDeleteProject() {
+    if (window.confirm('Do you really want to delete this project?')) {
+      Meteor.call('deleteProject', this.props.project._id);
+    }
+  },
+
   render() {
     return (
-      <div className="panel__content">Settings</div>
+      <div className="panel__content">
+        <button className="full-width negative" onClick={this.handleDeleteProject}>
+          Delete Project
+        </button>
+      </div>
     );
   }
 });
+
+if(Meteor.isServer) {
+  Meteor.methods({
+    deleteProject(id) {
+      Projects.remove(id);
+    }
+  });
+}
