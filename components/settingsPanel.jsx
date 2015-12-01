bea1150980f0ab7659508b1a057a447a0016c8c2
Andrew Liebchen <andrewliebchen@gmail.com>
2015-11-30 19:16:21 -0800

    Query params for all panels

--- components/settingsPanel.jsx
+++ components/settingsPanel.jsx
@@ -1,13 +1,28 @@
 SettingsPanel = React.createClass({
-  mixins: [DeleteProjectMixin],
+  handleDeleteProject(event) {
+    event.stopPropagation();
+
+    if (window.confirm('Do you really want to delete this project?')) {
+      Meteor.call('deleteProject', this.props.project._id);
+    }
+  },
 
   render() {
     return (
       <div className="panel__content">
         <button className="full-width negative" onClick={this.handleDeleteProject}>
-          Delete Project
+          <Icon type="trash" size={1.5}/> Delete Project
         </button>
       </div>
     );
   }
 });
+
+if(Meteor.isServer) {
+  Meteor.methods({
+    deleteProject(id) {
+      check(id, String);
+      Projects.remove(id);
+    }
+  });
+}
