37f8b748711001bbdd3f4d50ed344ee64b9ae8c1
Andrew Liebchen <andrewliebchen@gmail.com>
2015-12-04 16:03:25 -0800

    Add toasts

---
+++ components/toast.jsx
@@ -0,0 +1,29 @@
+const CSSTransitionGroup = React.addons.CSSTransitionGroup;
+
+Toast = React.createClass({
+  mixins: [ReactMeteorData],
+
+  getMeteorData() {
+    return {
+      toast: Session.get('toast')
+    };
+  },
+
+  componentDidUpdate() {
+    if(this.data.toast) {
+      setTimeout(() => {
+        Session.set('toast', false);
+      }, 5000);
+    }
+  },
+
+  render() {
+    return (
+      <CSSTransitionGroup transitionName="toast">
+        {this.data.toast ?
+          <div className="toast">{this.data.toast}</div>
+        : null}
+      </CSSTransitionGroup>
+    );
+  }
+});
